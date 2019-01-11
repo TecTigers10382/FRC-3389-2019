0c19c0eb8711545b261200cd3050e18ae6276344
Nathan <nathansd1098@gmail.com>
2019-01-10 21:47:42 -0500

    Revert "Added JavaDocs Comments to ExampleCommand"
    This reverts commit 9b1cbfc5a9fb00b0749f17b76be720d657fc0d3e.
    Realized there's already really good javadocs in the class it's extending

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -17,45 +17,34 @@ import frc.robot.Robot;
  */
 public class ExampleCommand extends Command {
   public ExampleCommand() {
-    // Subsystem Dependencies
+    // Use requires() here to declare subsystem dependencies
     requires(Robot.m_subsystem);
   }
 
-  /**
-   * Called just before this Command runs the first time
-   */
+  // Called just before this Command runs the first time
   @Override
   protected void initialize() {
   }
 
-  /**
-   * Called repeatedly when this Command is scheduled to run
-   */
+  // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
   }
 
-  /**
-   * Make this return true when this Command no longer needs to run execute()
-   */
+  // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
     return false;
   }
 
-  /**
-   * Called once after isFinished returns true
-   */
+  // Called once after isFinished returns true
   @Override
   protected void end() {
   }
 
-  /**
-   * Called when another command which requires one or more of the same
-   * subsystems is scheduled to run
-   */
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
   @Override
   protected void interrupted() {
-    
   }
 }
