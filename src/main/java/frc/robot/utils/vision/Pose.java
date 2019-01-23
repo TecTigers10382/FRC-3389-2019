179052a3ffbd29a409763576c83f4bb8fb4cc796
Nathan <nathansd1098@gmail.com>
2019-01-22 22:22:42 -0500

    added pose getters and setters

--- src/main/java/frc/robot/utils/vision/Pose.java
+++ src/main/java/frc/robot/utils/vision/Pose.java
@@ -23,4 +23,53 @@ public class Pose {
 		this.beta = beta;
 		this.gamma = gamma;
 	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
+	public double getAlpha() {
+		return alpha;
+	}
+
+	public void setAlpha(double alpha) {
+		this.alpha = alpha;
+	}
+
+	public double getBeta() {
+		return beta;
+	}
+
+	public void setBeta(double beta) {
+		this.beta = beta;
+	}
+
+	public double getGamma() {
+		return gamma;
+	}
+
+	public void setGamma(double gamma) {
+		this.gamma = gamma;
+	}
+
 }
