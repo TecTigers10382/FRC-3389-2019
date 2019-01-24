e5037496f7094aa0bede7a5fe30172c8da52e1e0
Nathan <nathansd1098@gmail.com>
2019-01-23 22:46:15 -0500

    Wrote method to convert target location to required displacement of each wheel

--- src/main/java/frc/robot/utils/VisionCargoBay.java
+++ src/main/java/frc/robot/utils/VisionCargoBay.java
@@ -202,4 +202,102 @@ public class VisionCargoBay {
 		double yPrimeC = -targetC.x * Math.sin(yaw) + targetC.y * Math.cos(yaw);
 		return yPrimeC;
 	}
+
+	/**
+	 * Emprical value found by measuring the percent error when travelling fowards.
+	 * 
+	 * Example: Drive 36in and measure real value. a_x = real value/36in. Find the
+	 * average over multiple trials will make it better.
+	 */
+	final double a_x = 1;
+
+	/**
+	 * Emprical value found by measuring the percent error when travelling sideways.
+	 * 
+	 * Example: Drive 36in and measure real value. a_r = real value/36in. Find the
+	 * average over multiple trials will make it better.
+	 */
+	final double a_r = 1;
+
+	/**
+	 * Emprical value found by measuring the percent error when rotating.
+	 * 
+	 * Example: Rotate 720deg and measure real value. a_z = real value/720deg. Find
+	 * the average over multiple trials will make it better.
+	 */
+	final double a_z = 1;
+
+	/**
+	 * Radius of mecanum wheels.
+	 */
+	final double MECANUM_RADIUS = 3;
+
+	/**
+	 * Distance of mecanum wheel from center of gravity in forwards backwards
+	 * direction.
+	 */
+	final double Y_OFFSET = 29 / 2;
+
+	/**
+	 * Distance of mecanum wheel from center of gravity in forwards left right
+	 * direction.
+	 */
+	final double X_OFFSET = 29 / 2;
+
+	/**
+	 * Gives required displacements of each wheel to reach desired target relative
+	 * to robot.
+	 * 
+	 * Coordinate System: </br>
+	 * Origin is center of robot. </br>
+	 * y+ is forwards from the origin. </br>
+	 * x+ is right from the origin. </br>
+	 * CCW rotation is positive. </br>
+	 * 
+	 * @param x   x-coordinate relative to robot in inches.
+	 * @param y   y-coordinate relative to robot in inches.
+	 * @param rot angle to rotate relative to robot in degrees. -2pi<rot<2pi
+	 * @return How many radians each wheel must rotate to reach desired location
+	 */
+	public double[] mecanumPath(double x, double y, double rot) {
+		// Converts incoming coordinates to the coordinates needed in the math.
+		// Coordinates in math come from this paper:
+		// https://repository.tudelft.nl/islandora/object/uuid:6b6cd917-8a03-4448-903e-33d4428d3831/datastream/OBJ/download
+		double temp = y;
+		y = -x;
+		x = temp;
+		rot = -Math.toRadians(rot);
+
+		// To avoid divide by 0 error (basically a limit)
+		if (rot == 0)
+			rot = 0.000001;
+
+		// This is actually just magic don't look at it.
+		double FL = -(rot * (a_z * y * Math.cos(rot) - 2 * Y_OFFSET * a_r - a_z * y - 2 * X_OFFSET * a_r
+				+ a_z * y * Math.sin(rot) + a_r * a_z * x + 2 * X_OFFSET * a_r * Math.cos(rot)
+				+ 2 * Y_OFFSET * a_r * Math.cos(rot) - a_r * a_z * x * Math.cos(rot) + a_r * a_z * x * Math.sin(rot)))
+				/ (2 * MECANUM_RADIUS * a_x * a_r * a_z * (Math.cos(rot) - 1));
+
+		double FR = -(rot * (2 * X_OFFSET * a_r + 2 * Y_OFFSET * a_r - a_z * y + a_z * y * Math.cos(rot)
+				- a_z * y * Math.sin(rot) - a_r * a_z * x - 2 * X_OFFSET * a_r * Math.cos(rot)
+				- 2 * Y_OFFSET * a_r * Math.cos(rot) + a_r * a_z * x * Math.cos(rot) + a_r * a_z * x * Math.sin(rot)))
+				/ (2 * MECANUM_RADIUS * a_x * a_r * a_z * (Math.cos(rot) - 1));
+
+		double RL = (rot * (2 * X_OFFSET * a_r + 2 * Y_OFFSET * a_r + a_z * y - a_z * y * Math.cos(rot)
+				+ a_z * y * Math.sin(rot) + a_r * a_z * x - 2 * X_OFFSET * a_r * Math.cos(rot)
+				- 2 * Y_OFFSET * a_r * Math.cos(rot) - a_r * a_z * x * Math.cos(rot) - a_r * a_z * x * Math.sin(rot)))
+				/ (2 * MECANUM_RADIUS * a_x * a_r * a_z * (Math.cos(rot) - 1));
+
+		double RR = -(rot * (2 * X_OFFSET * a_r + 2 * Y_OFFSET * a_r - a_z * y + a_z * y * Math.cos(rot)
+				+ a_z * y * Math.sin(rot) + a_r * a_z * x - 2 * X_OFFSET * a_r * Math.cos(rot)
+				- 2 * Y_OFFSET * a_r * Math.cos(rot) - a_r * a_z * x * Math.cos(rot) + a_r * a_z * x * Math.sin(rot)))
+				/ (2 * MECANUM_RADIUS * a_x * a_r * a_z * (Math.cos(rot) - 1));
+
+		double[] out = new double[4];
+		out[0] = FL;
+		out[1] = FR;
+		out[2] = RL;
+		out[3] = RR;
+		return out;
+	}
 }
