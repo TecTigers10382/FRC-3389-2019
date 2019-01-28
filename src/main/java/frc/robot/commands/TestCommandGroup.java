f9c3fd9e72cd5cdcada81dac77caab965befad3e
TEC Tigers <wicomicocountyroboticsclub@gmail.com>
2019-01-28 18:56:32 -0500

    Created test command group for auto alignment code

---
+++ src/main/java/frc/robot/commands/TestCommandGroup.java
@@ -0,0 +1,23 @@
+/*
+* Copyright (c) 2017-2018 FRC TEAM 3389. All Rights Reserved.
+* Open Source Software - may be modified and shared by FRC teams. The code
+* must be accompanied by the FIRST BSD license file in the root directory of
+* the project.
+*/
+
+package frc.robot.commands;
+
+import frc.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class TestCommandGroup extends CommandGroup {
+	// private properties here
+
+	public TestCommandGroup() {
+		// addSequential(new Command());
+
+		// addParallel(new Command());
+		// addSequential(new Command());
+	}
+}
