dd04890e7ea840a21bfa1b3c113aa8a20695cfd6
TEC Tigers <wicomicocountyroboticsclub@gmail.com>
2019-02-08 18:27:42 -0500

    Made AutoLift command

---
+++ src/main/java/frc/robot/commands/AutoLift.java
@@ -0,0 +1,69 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+/**
+ * Put description here.
+ *
+ * @author FRC Team 3389 TEC Tigers
+ */
+public class AutoLift extends Command {
+	TalonSRX liftR, liftL;
+	Potentiometer pot;
+
+	public AutoLift() {
+		liftL = new TalonSRX(RobotMap.LIFT_LEFT);
+		liftR = new TalonSRX(RobotMap.LIFT_RIGHT);
+
+		liftL.follow(liftR);
+
+		liftR.setInverted(false);
+		liftL.setInverted(InvertType.OpposeMaster);
+
+		liftL.getSelectedSensorPosition();
+		// pot = new AnalogPotentiometer(RobotMap.POT_INPUT, 360, 0);
+
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+}
